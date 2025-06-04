843e66da719784002a4670ba1e2d8ec66eed90ca
codingTrip <codingtrip@naver.com>
2025-06-04 12:19:34 +0900

    3. 객체 지향 프로그래밍 - 객체 지향 프로그래밍

--- src/oop1/MusicPlayerMain3.java
+++ src/oop1/MusicPlayerMain3.java
@@ -26,32 +26,32 @@ public class MusicPlayerMain3 {
         off(data);
     }
 
-    private static void showStatus(MusicPlayerData data) {
+    static void on(MusicPlayerData data) {
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다");
+    }
+
+    static void off(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다");
+    }
+
+    static void volumeUp(MusicPlayerData data) {
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+
+    static void volumeDown(MusicPlayerData data) {
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+
+    static void showStatus(MusicPlayerData data) {
         System.out.println("음악 플레이어 상태 확인");
         if (data.isOn) {
             System.out.println("음악 플레이어 ON, 볼륨:" + data.volume);
         } else {
             System.out.println("음악 플레이어 OFF");
         }
-    }
-
-    private static void on(MusicPlayerData data) {
-        data.isOn = true;
-        System.out.println("음악 플레이어를 시작합니다");
-    }
-
-    private static void off(MusicPlayerData data) {
-        data.isOn = false;
-        System.out.println("음악 플레이어를 종료합니다");
-    }
-
-    private static void volumeUp(MusicPlayerData data) {
-        data.volume++;
-        System.out.println("음악 플레이어 볼륨:" + data.volume);
-    }
-
-    private static void volumeDown(MusicPlayerData data) {
-        data.volume--;
-        System.out.println("음악 플레이어 볼륨:" + data.volume);
     }
 }
