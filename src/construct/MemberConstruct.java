f52f7867a1eb712950be574ba9b0bb5044325e26
codingTrip <codingtrip@naver.com>
2025-06-06 17:44:29 +0900

    4. 생성자 - 생성자 - 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,15 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age) {
+//        System.out.println("go"); 컴파일 오류 발생
+        this(name, age, 50); //변경
+        /*this.name = name;
+        this.age = age;
+        this.grade = 50;*/
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ",age=" + age + ",grade=" + grade);
         this.name = name;
